b2a1951cbf540ecf3fd34ceedbabc9ea98f57ec8
whitney-shisia <shisiawhitney215@gmail.com>
2024-07-31 07:25:22 +0300

    c

---
+++ file.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int myNum = 15;
+    float myFloatNum = 13.5;
+    char myLetter = 'D';
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
+    return 0;
+}
+
